1cc5c437065b77545839c578362fa44b12c92488
Connor Nielsen <cwnielse@calpoly.edu>
2014-09-14 22:11:55 -0700

    roger roger

---
+++ assets/js/src/app.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+
+	setTimeout(function(){
+	$(".logo").addClass("shine");
+		setTimeout(function(){
+			$(".header-top").addClass("shine");
+			$(".tagline").fadeIn(3000);
+		}, 500);
+
+		var $menu = $(".mobileactionbar");
+
+	    $(document).scroll(function() {
+	        if (300 <= $(document).scrollTop()) {
+	            $menu.addClass("shine");
+	            $('body').addClass("bg");
+	        } else {
+	        	$menu.removeClass("shine");
+	        }
+	    });
+	}, 1000);
+
+
+});
